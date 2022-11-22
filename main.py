8bb244ba3a01d0e4b870cd3eab599bc98874ac8f
Aleno <aleno.jankowsky@5minds.de>
2022-11-22 10:46:17 +0100

    Add main while loop for playing the game

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from HangmanStages import stages
+from Words import word_list
+from random import choice
+
 
 def print_hangman_stage(stage):
     print(stages[stage])
 
-print_hangman_stage(0)
+
+def get_random_word_from_list() -> str:
+    return choice(word_list).upper()
+
+
+def play(random_word):
+    remaining_tries = 6
+    word_was_guessed = False
+    while not word_was_guessed and remaining_tries > 0:
+        user_guess = input("Bitte geben Sie einen Buchstaben oder Wort ein: ").upper()
+        if user_guess not in random_word:
+            print(
+                f"Der Buchstabe oder das Wort {user_guess} ist nicht im Ratewort enthalten"
+            )
+            remaining_tries -= 1
+        else:
+            print(f"Der Buchstabe oder das Wort {user_guess} ist im Ratewort enthalten")
+
+
+def main_game():
+    play(get_random_word_from_list())
+
+
+if __name__ == "__main__":
+    main_game()
